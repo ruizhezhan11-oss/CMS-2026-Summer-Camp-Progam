70dae4fa80e81509556924d8fbc68091b0676163
ruizhezhan11-oss <ruizhezhan11@gmail.com>
2026-07-12 13:53:26 +0800

    Update Team1.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team1.java
@@ -78,15 +78,16 @@ public class Team1 extends LinearOpMode {
         //Press "START"
         if (opModeIsActive()) {
             // 一次性程式
-            telemetry.addLine("成功啟動OpMode");
+
             //======無限迴圈======\\
             while (opModeIsActive()) {
                 double shooter1RPM = (shooter.getVelocity() / CPR) * 60.0;
                 double shooter2RPM = (shooter2.getVelocity() / CPR) * 60.0;
-                telemetry.addLine("Shooter一律進戰模式:3750轉");
-                telemetry.addData("Shooter1即時轉速", shooter1RPM);
-                telemetry.addData("Shooter2及時轉速", shooter2RPM);
-                telemetry.addData("自動連招模式", autoFireOn ? "🟢 開啟" : "⚪ 關閉");
+                telemetry.addLine("成功啟動OpMode");
+                telemetry.addData("Shooter目標",targetRPM +"轉");
+                telemetry.addData("Shooter1即時轉速", shooter1RPM +"RPM");
+                telemetry.addData("Shooter2及時轉速", shooter2RPM +"RPM");
+
                 // While迴圈
                 FL.setPower(-gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x);
                 FR.setPower((-gamepad1.left_stick_y - gamepad1.left_stick_x) - gamepad1.right_stick_x);
@@ -107,7 +108,6 @@ public class Team1 extends LinearOpMode {
 
                 if(autoFireOn){
                     //自動程序
-                    //先等待吸球
                     intake.setPower(1);
 
                     double ticksPerSecond = (targetRPM / 60.0) * CPR;
@@ -139,6 +139,7 @@ public class Team1 extends LinearOpMode {
                     if (gamepad1.x) {
                         stopAllMotors();
                     }
+                    telemetry.update();
 
                 }
             }
