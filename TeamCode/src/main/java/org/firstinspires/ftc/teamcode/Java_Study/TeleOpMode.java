19cd31378c117d03fa37c965a6b8d7600035a660
ruizhezhan11-oss <ruizhezhan11@gmail.com>
2026-07-15 13:55:53 +0800

    Update TeleOpMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Java_Study/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Java_Study/TeleOpMode.java
@@ -12,23 +12,41 @@ public class TeleOpMode extends LinearOpMode {
     int Sixseven = 67;
     double abc = 3.14159;
     DcMotor motor1 ;
-    DcMotorEx motor2;
+    DcMotorEx arm;
     Servo servo ;
 
     @Override
     public void runOpMode()throws InterruptedException{
+        init_hardware();
         waitForStart();
         while(opModeIsActive()){
         motor1.setPower(add(gamepad1.left_stick_x,gamepad1.left_stick_y));//呼叫函式
+            if(gamepad1.a){
+                motor1.setPower(1);
+            }
+            if(gamepad1.b){
+                arm.setTargetPosition(12000);
+                arm.setPower(0.6);
+            }else{
+                arm.setPower(0);
+            }
+
         }
     }
     //建立涵式
-    public double add(double a,double b){
+    private double add(double a,double b){
         return a+b;
     }
-    public void init_hardware(){
-        //!初始化硬體
-        //Todo
+    private void init_hardware(){
+        motor1 = hardwareMap.get(DcMotor.class,"motor1");
+        arm = hardwareMap.get(DcMotorEx.class,"arm");
+        servo = hardwareMap.get(Servo.class,"servo");
+        motor1.setDirection(DcMotor.Direction.FORWARD);
+        arm.setDirection(DcMotor.Direction.REVERSE);
+        servo.setDirection(Servo.Direction.FORWARD);
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
 }
